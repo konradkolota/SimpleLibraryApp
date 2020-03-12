a9ee1f9ad065dc079cae200205878db3805cdd1a
konradkolota <konradkolota@gmail.com>
2020-03-12 11:57:05 +0100

    Task-3. Initialized data with Spring.

--- src/main/java/com/kk/springapp/domain/Book.java
+++ src/main/java/com/kk/springapp/domain/Book.java
@@ -1,6 +1,7 @@
 package com.kk.springapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,7 +17,7 @@ public class Book {
 
     @ManyToMany
     @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
